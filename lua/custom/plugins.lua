e9db41c4c076935c111e270b4174a7c10ffb1dde
I-Want-ToBelieve <floatshuyin@gmail.com>
2023-07-20 21:33:39 +0800

    update

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,8 @@
 local overrides = require("custom.configs.overrides")
 
+-- @see https://github.com/vscode-neovim/vscode-neovim#neovim-configuration
+local is_vscode = vim.g.vscode == 1
+
 ---@type NvPluginSpec[]
 local plugins = {
 
@@ -7,6 +10,7 @@ local plugins = {
 
   {
     "neovim/nvim-lspconfig",
+    enabled = not is_vscode,
     dependencies = {
       -- format & linting
       {
@@ -36,6 +40,7 @@ local plugins = {
   {
     "nvim-tree/nvim-tree.lua",
     opts = overrides.nvimtree,
+    enabled = not is_vscode,
   },
 
   -- Install a plugin
@@ -47,12 +52,56 @@ local plugins = {
     end,
   },
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+  -- if is vscode then to make a plugin not be loaded
+  {
+    "NvChad/base46",
+    enabled = not is_vscode,
+  },
 
+  {
+    "NvChad/ui",
+    enabled = not is_vscode,
+  },
+
+  {
+    "NvChad/nvterm",
+    enabled = not is_vscode,
+  },
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    enabled = not is_vscode,
+  },
+
+  {
+    "nvim-tree/nvim-web-devicons",
+    enabled = not is_vscode,
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = not is_vscode,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = not is_vscode,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = not is_vscode,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    enabled = not is_vscode,
+  },
+
+  {
+    "folke/which-key.nvim",
+    enabled = not is_vscode,
+  },
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
   -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
